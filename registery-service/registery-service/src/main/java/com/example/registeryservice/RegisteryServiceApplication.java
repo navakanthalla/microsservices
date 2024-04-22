8eadd4128a80c41d11f3b2cd28f5de52f4f660ea
navak <navak@DESKTOP-EV92M0U>
2024-04-22 18:58:10 +0530

    initial commit

---
+++ registery-service/registery-service/src/main/java/com/example/registeryservice/RegisteryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.registeryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisteryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisteryServiceApplication.class, args);
+	}
+
+}
